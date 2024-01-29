f645372f04918b46bfa4c9c90fc19ee71db2c015
Ian <ian.hanson.r@gmail.com>
2024-01-29 15:50:12 -0800

    Add CreditCard method for mock payment.

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import pandas as pd
 
 filepath = "hotels.csv"
 df = pd.read_csv(filepath, dtype={"id": str})
+df_card = pd.read_csv("cards.csv", dtype=str).to_dict(orient="records")
 
 
 class Hotel:
     def __init__(self, hotel_id_local):
         self.hotel_id = hotel_id_local
-        self.name = df.loc[df["id"] == self.hotel_id, "name"]
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
 
     def book(self):
         df.loc[df["id"] == self.hotel_id, "available"] = "no"
         df.to_csv(filepath, index=False)
-        print("Hotel Booked!")
+        print("Hotel Reserved")
 
     def available(self):
         availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
@@ -31,9 +32,22 @@ class ReservationTicket:
         content = f"""
         Thank you for booking with us!
         Customer Name: {self.customer_name}
-        Hotel: {self.hotel.name}
-"""
+        Hotel: {self.hotel.name}"""
         return content
+
+
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {"number": self.number, "expiration": expiration, "holder": holder, "cvc": cvc}
+        if card_data in df_card:
+            print("Payment Pending")
+            return True
+        else:
+            print("Invalid Payment. Please try another.")
+            return False
 
 
 print(df)
@@ -41,6 +55,8 @@ hotel_id = input("Enter hotel ID: ")
 hotel = Hotel(hotel_id)
 
 if hotel.available():
+    credit_card = CreditCard(number="1234567890123456")
+    credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123")
     hotel.book()
     name = input("Enter your name: ")
     reservation_ticket = ReservationTicket(customer_name_local=name, hotel_object=hotel)
